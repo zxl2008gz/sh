c93abc626cd3842f4095177eb895daa54d3435fd
zxl2008gz <55257260+zxl2008gz@users.noreply.github.com>
2024-01-02 21:42:38 +0800

    Update solin.sh

--- solin.sh
+++ solin.sh
@@ -123,3 +123,36 @@ install_docker() {
         echo "Docker 已经安装"
     fi
 }
+
+
+
+while true; do
+clear
+
+echo -e "\033[96m_  _ ____  _ _ _    _ ____ _  _ "
+echo "|_  |___  | | |    | |  | |\ | "
+echo "  | _ |___ _| | |___ | |__| | \| "
+echo "                                "
+echo -e "\033[96m科技lion一键脚本工具 v2.1.5 （支持Ubuntu/Debian/CentOS系统）\033[0m"
+echo -e "\033[96m-输入\033[93mk\033[96m可快速启动此脚本-\033[0m"
+echo "------------------------"
+echo "1. 系统信息查询"
+echo "2. 系统更新"
+echo "3. 系统清理"
+echo "4. 常用工具 ▶"
+echo "5. BBR管理 ▶"
+echo "6. Docker管理 ▶ "
+echo "7. WARP管理 ▶ 解锁ChatGPT Netflix"
+echo "8. 测试脚本合集 ▶ "
+echo "9. 甲骨文云脚本合集 ▶ "
+echo -e "\033[33m10. LDNMP建站 ▶ \033[0m"
+echo "11. 面板工具 ▶ "
+echo "12. 我的工作区 ▶ "
+echo "13. 系统工具 ▶ "
+echo "------------------------"
+echo "00. 脚本更新"
+echo "------------------------"
+echo "0. 退出脚本"
+echo "------------------------"
+read -p "请输入你的选择: " choice
+
