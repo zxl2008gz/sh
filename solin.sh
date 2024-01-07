ba40e08f5bed01393ac87011c6d16a9bd1b4c0a5
zxl2008gz <55257260+zxl2008gz@users.noreply.github.com>
2024-01-06 22:51:19 -0600

    Update solin.sh

--- solin.sh
+++ solin.sh
@@ -1305,7 +1305,7 @@ while true; do
 									clear
 									echo "获取license_id请访问: https://dash.pandoranext.com/"
 									read -p "请输入你的GitHub的license_id: " github1
-									read -p "请输入你的网页登录界面: " site_pswd
+									read -p "请设置你的网页登录界面密码: " site_pswd
 
 									install_docker
 
@@ -1313,10 +1313,10 @@ while true; do
 									cd /home/docker/PandoraNext/data
 									wget https://raw.githubusercontent.com/zxl2008gz/docker/main/PandoraNext/config.json									
 									wget https://raw.githubusercontent.com/zxl2008gz/docker/main/PandoraNext/tokens.json
+									sed -i "s/site_pswd123/$site_pswd/g" /home/docker/PandoraNext/data/tokens.json
 									sed -i "s/github/$github1/g" /home/docker/PandoraNext/data/config.json
 									webgptpasswd1=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c16)
-									sed -i "s/webgptpasswd/$webgptpasswd1/g" /home/docker/PandoraNext/data/config.json
-									sed -i "s/site_pswd123/$site_pswd/g" /home/docker/PandoraNext/data/tokens.json
+									sed -i "s/webgptpasswd/$webgptpasswd1/g" /home/docker/PandoraNext/data/config.json									
 
 									$docker_rum
 									duankou=8181
