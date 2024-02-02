9211850e9dcaeeba1dad5da5819db9aae5cce654
zxl2008gz <55257260+zxl2008gz@users.noreply.github.com>
2024-02-02 15:25:46 -0600

    Update solin.sh

--- solin.sh
+++ solin.sh
@@ -4066,7 +4066,17 @@ while true; do
         7)
             clear
             system_tool
-            ;;        
+            ;; 
+        00)
+            cd ~
+            #curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/update_log.sh && chmod +x update_log.sh && ./update_log.sh
+            #rm update_log.sh
+            echo ""
+            curl -sS -O https://raw.githubusercontent.com/zxl2008gz/sh/main/solin.sh && chmod +x solin.sh && ./solin.sh
+            echo "脚本已更新到最新版本！"
+            break_end
+            solin
+            ;;       
         0)
             # 退出脚本
             clear
