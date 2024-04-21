dcb90d2689af9eb7b61cfa687666ce82f625df9e
zxl2008gz <55257260+zxl2008gz@users.noreply.github.com>
2024-04-21 00:08:50 -0700

    Update monitor_env.sh

--- monitor_env.sh
+++ monitor_env.sh
@@ -3,22 +3,27 @@
 # 脚本参数：传递域名作为第一个参数
 domain="$1"
 env_path="/home/docker/html/$domain/dujiaoka/.env"
-last_hash=$(md5sum "$env_path" | cut -d ' ' -f1)
+backup_path="$env_path.bak"
 logfile="/var/log/env_monitor.log"
 
 # 确保日志文件存在
 touch "$logfile"
 
+# 创建.env文件的备份
+cp "$env_path" "$backup_path"
+
 # 监控.env文件的修改
 echo "正在监控文件变化 $env_path" >> "$logfile"
 inotifywait -m -e modify "$env_path" | while read path action file; do
     current_hash=$(md5sum "$env_path" | cut -d ' ' -f1)
+    last_hash=$(md5sum "$backup_path" | cut -d ' ' -f1)
     if [ "$current_hash" != "$last_hash" ]; then
-        sed -i 's/^ADMIN_HTTPS=false/ADMIN_HTTPS=true/g' "$env_path"
-        echo "$(date) - 检测到 $file 被 $action，ADMIN_HTTPS 设置已更新为 true" >> "$logfile"
-        last_hash=$current_hash
+        if grep -q "^ADMIN_HTTPS=false" "$env_path"; then
+            sed -i 's/^ADMIN_HTTPS=false/ADMIN_HTTPS=true/g' "$env_path" && \
+            echo "$(date) - 检测到 $file 被 $action，ADMIN_HTTPS 设置已更新为 true" >> "$logfile" || \
+            echo "$(date) - 尝试更新 $file 时发生错误" >> "$logfile"
+        fi
+        # 更新备份文件
+        cp "$env_path" "$backup_path"
     fi
 done &
-
-# 将后台进程PID写入日志
-echo $! > "/var/run/env_monitor.pid"
