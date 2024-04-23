b43ec6284c09585e4cd6773f6e946a8a03f4ae95
zxl2008gz <55257260+zxl2008gz@users.noreply.github.com>
2024-04-23 00:45:38 -0700

    Update mysql_db_manager.sh

--- mysql_db_manager.sh
+++ mysql_db_manager.sh
@@ -431,14 +431,14 @@ case "$1" in
     create)
         container_name1="$2"
 	dbname="$3"
-        container_name_mysql=$(get_db_container_name "$container_name1")
+        container_name_mysql=$(select_db_container "$container_name1")
         credentials=($(get_db_credentials "$container_name_mysql"))
         create_database_and_grant "$container_name_mysql" "$dbname" "${credentials[0]}" "${credentials[1]}" "${credentials[2]}"
         ;;
     delete)
         container_name1="$2"
 	dbname="$3"
-        container_name_mysql=$(get_db_container_name "$container_name1")
+        container_name_mysql=$(select_db_container "$container_name1")
         credentials=($(get_db_credentials "$container_name_mysql"))
         delete_database "$container_name_mysql" "$dbname" "${credentials[2]}" "${credentials[0]}"
         ;;
